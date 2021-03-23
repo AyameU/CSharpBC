f81c03d8787eb552f804ee6fe9e4f4c11647d299
Ayame Ulrich <54376512+AyameU@users.noreply.github.com>
2021-03-22 20:12:13 -0500

    Trimmed code to fit within Discord's 2000 character limit. Bye bye documentation :/

--- homework/lesson_1/lesson_1/Program.cs
+++ homework/lesson_1/lesson_1/Program.cs
@@ -8,89 +8,49 @@ namespace lesson_1
         {
             // Person 1
             GetUserInfo();
-
             // Person 2
             GetUserInfo();
-
             // Person 3
             GetUserInfo();
         }
 
-        /// <summary>
-        /// Prints the calculated BMI to the console.
-        /// </summary>
-        static void PrintBMIToConsole(string name, double bmi)
+        static void PrintBMI(string name, double bmi)
         {
             Console.WriteLine($"{name}'s BMI is {bmi:n}.\n");
         }
 
-        /// <summary>
-        /// Calculates a person's body mass index.
-        /// </summary>
-        static double CalculateBodyMassIndex(double weight, double height)
+        static double CalculateBMI(double weight, double height)
         {
-            double heightInMetres = height / 100;
-            double bmi = weight / (heightInMetres * heightInMetres);
+            double metres = height / 100;
+            double bmi = weight / (metres * metres);
             return bmi;
         }
 
-        /// <summary>
-        /// Prints the user's info to the console.
-        /// </summary>
-        static void PrintUserInfoToConsole(string name, string surname, string age, double weight, double height)
+        static void PrintUserInfo(string name, string surname, string age, double weight, double height)
         {
             Console.WriteLine($"{name} {surname} is {age} years old, they are {height} cm tall and weigh {weight} kg.");
         }
 
-        /// <summary>
-        /// Gets the person's info from the console.
-        /// </summary>
         static void GetUserInfo()
         {
             string name = ReadFromConsole("name");
             string surname = ReadFromConsole("surname");
-
             string age = ReadFromConsole("age");
-
-            string weightInput = ReadMeasurementsFromConsole("weight", "kg");
-            double weight = ConvertStringToDouble(weightInput);
-
-            string heightInput = ReadMeasurementsFromConsole("height", "cm");
-            double height = ConvertStringToDouble(heightInput);
-
-            PrintUserInfoToConsole(name, surname, age, weight, height);
-
-            double bmi = CalculateBodyMassIndex(weight, height);
-            PrintBMIToConsole(name, bmi);
+            string weightInput = ReadFromConsole("weight in kg");
+            double weight = ConvertToDouble(weightInput);
+            string heightInput = ReadFromConsole("height in cm");
+            double height = ConvertToDouble(heightInput);
+            PrintUserInfo(name, surname, age, weight, height);
+            PrintBMI(name, CalculateBMI(weight, height));
         }
 
-        /// <summary>
-        /// Takes user input from the console of a specified type.
-        /// </summary>
-        /// <returns>The user input.</returns>
         static string ReadFromConsole(string inputType)
         {
             Console.Write($"Please enter a(n) {inputType}: ");
-            string input = Console.ReadLine();
-            return input;
+            return Console.ReadLine();
         }
 
-        /// <summary>
-        /// Takes user input from the console of the specified type and unit of measurement.
-        /// </summary>
-        /// <returns>The user input.</returns>
-        static string ReadMeasurementsFromConsole(string inputType, string unitOfMeasurement)
-        {
-            Console.Write($"Please enter a(n) {inputType} in {unitOfMeasurement}: ");
-            string input = Console.ReadLine();
-            return input;
-        }
-
-        /// <summary>
-        /// Converts a string to a double.
-        /// </summary>
-        /// <returns>The converted double.</returns>
-        static double ConvertStringToDouble(string input)
+        static double ConvertToDouble(string input)
         {
             double converted = double.Parse(input);
             return converted;
